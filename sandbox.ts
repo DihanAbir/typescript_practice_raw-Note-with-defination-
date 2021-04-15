617b824b7a1aae5043298e6b495cd9773fdacb0d
NAHID MURAD ABIR <nahid.muradabir@gmail.com>
2021-04-15 11:00:52 +0600

    added starter comand

--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,12 @@
+// starter basic informations: 
+
+// 1. add a ts file 
+// 2. to convert ts to js, use this command : tsc file names 
+// 3. if both ts file and js file same, then convert ts to js by typing this command: "tsc filenam.ts" 
+// 4. for auto start when press the save key without reload, use watch flag after all the necessary command: tsc sandbox.ts -w
+
+
+
 const character = 'dihan'
 
 console.log(character);
