09ee13f4522e97055d27221fbc086c241daf3ea5
NAHID MURAD ABIR <nahid.muradabir@gmail.com>
2021-04-15 10:54:32 +0600

    initial ts project and add ts config with flag -w !

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var character = 'dihan';
+console.log(character);
+var input = document.querySelectorAll('input');
+console.log(input);
+console.log('ok');
+input.forEach(function (value) {
+    console.log(value);
+});
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+const character = 'dihan'
+
+console.log(character);
+
+const input = document.querySelectorAll('input');
+console.log(input);
+console.log('ok');
+
+input.forEach(value => {
+    console.log(value);
+    
+})
